07856c69a11c1650b215b3e01bd623467bb292f8
Phillip Wong <pwong@paybyphone.com>
2019-04-18 16:25:29 -0700

    Added in test for API key newline sanitisation.

--- plugin/providers/keboola/provider_test.go
+++ plugin/providers/keboola/provider_test.go
@@ -1,9 +1,11 @@
 package keboola
 
 import (
+	"log"
 	"os"
 	"testing"
 
+	"github.com/hashicorp/terraform/config"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -26,9 +28,29 @@ func TestProvider(t *testing.T) {
 
 func TestProvider_ApiKey(t *testing.T) {
 	provider := Provider().(*schema.Provider)
+	c, _ := config.NewRawConfig(map[string]interface{}{
+		"api_key": "abcdefg\n\n\n",
+	})
 
-	t.Log("hello!")
-	t.Logf("%#v", provider)
+	provider.Configure(terraform.NewResourceConfig(c))
+
+	if provider == nil {
+		log.Print("[DEBUG] Unable to read account ID from test provider: empty provider")
+	}
+
+	if provider.Meta() == nil {
+		log.Print("[DEBUG] Unable to read account ID from test provider: unconfigured provider")
+	}
+
+	client, ok := provider.Meta().(*KBCClient)
+
+	if !ok {
+		log.Print("[DEBUG] Unable to read account ID from test provider: non-AWS or unconfigured AWS provider")
+	}
+
+	if client.APIKey != "abcdefg" {
+		t.Fatalf("err: %s", "API key still contains a newline, newlines should be stripped out in the terraform provider")
+	}
 }
 
 func TestProvider_impl(t *testing.T) {
@@ -40,8 +62,3 @@ func testAccPreCheck(t *testing.T) {
 		t.Fatal("STORAGE_API_KEY must be set for acceptance tests")
 	}
 }
-
-const testApiKey = `
-  provider "keboola" {
-	api_key = "abcdefg"
-  }`
